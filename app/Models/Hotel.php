1733b0450c1825100fc14dc26a98f44fcd312009
shwephuehmone25 <shwephue7889@gmail.com>
2025-02-26 15:35:02 +0630

    add scope filter for hotels

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Hotel extends Model
 {
@@ -26,5 +27,15 @@ class Hotel extends Model
     public function bookings()
     {
         return $this->hasMany(HotelBooking::class);
+    }
+
+    public function scopeFilter(Builder $query, $search)
+    {
+        if ($search) {
+            $query->where(function ($q) use ($search) {
+                $q->where('name', 'like', "%{$search}%")
+                  ->orWhere('location', 'like', "%{$search}%");
+            });
+        }
     }
 }
